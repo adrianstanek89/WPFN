2739b57a49da183466a68505add096b57aeedd27
adrianstanek89 <31177062+adrianstanek89@users.noreply.github.com>
2018-02-07 10:44:45 +0100

    Add files via upload

---
+++ ProgressBar/MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.ComponentModel;
+using System.IO;
+using System.Net;
+using System.Windows;
+
+namespace ProgressBar
+{
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        WebClient webClient;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void DownloadFile(string urlAddress, string location)
+        {
+            using (webClient = new WebClient())
+            {
+                webClient.DownloadFileCompleted += new AsyncCompletedEventHandler(Completed);
+                webClient.DownloadProgressChanged += new DownloadProgressChangedEventHandler(ProgressChanged);
+
+                try
+                {
+                    webClient.DownloadFileAsync(new Uri(urlAddress), location);
+                }
+                catch (Exception ex)
+                {
+
+                    MessageBox.Show(ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+        }
+
+        private void ProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            progressBarDownload.Value = e.ProgressPercentage;
+            labelPerc.Text = e.ProgressPercentage.ToString() + "%";
+        }
+
+        private void Completed(object sender, AsyncCompletedEventArgs e)
+        {
+            if(e.Cancelled)
+            {
+                MessageBox.Show("Pobieranie zostalo przerwane", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            else
+            {
+                MessageBox.Show("Pobieranie zostalo szczesliwie zakonczone.", "Info", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+        }
+
+        private void buttonDownload_Click(object sender, RoutedEventArgs e)
+        {
+            if(!string.IsNullOrWhiteSpace(urlAddress.Text))
+            {
+                DownloadFile(urlAddress.Text, Path.Combine(Directory.GetCurrentDirectory(), $"file.pdf"));
+            }
+        }
+    }
+}
---
+++ WPFPodstawy/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WPFPodstawy;
+
+namespace ReservationEvent
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            EventHanlderReservation eventHanlderReservation = new EventHanlderReservation();
+            Reservation reservation = new Reservation();
+            reservation.ReservationDone += eventHanlderReservation.LogTrans;
+
+            reservation.Reserve("Loki", "Kozo wróć");
+            Console.ReadKey();
+        }
+    }
+}

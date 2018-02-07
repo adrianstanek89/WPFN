e62ddff4c16fac18f5bead12c143cf7d72dc03a3
adrianstanek89 <31177062+adrianstanek89@users.noreply.github.com>
2018-02-07 10:37:27 +0100

    Add files via upload

---
+++ ItemsSource/Extensions/StringExtension.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ItemsSource.Extensions
+{
+    public static class StringExtension
+    {
+        public static string MyExt(this string test, int asd)
+        {
+            return test.ToLower().Substring(0, asd);
+        }
+    }
+}
---
+++ ItemsSource/OurImage.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ItemsSource.Extensions;
+using System.IO;
+
+namespace ItemsSource
+{
+    public class OurImage
+    {        
+        public string DefaultName { get
+            {
+                return "Default";
+            }
+        }
+        public string PathOfImage { get; set; }
+        public string NameOfImage { get; set; }
+        public bool IsFound { get; set; }
+
+        public OurImage()
+        {
+            this.PathOfImage = "default.png";
+            this.NameOfImage = "Default";
+        }
+
+        public OurImage(string pathOfImage, string nameOfImage)
+        {
+            this.PathOfImage = pathOfImage;
+            this.NameOfImage = nameOfImage;
+            this.IsFound = File.Exists(Path.Combine(Directory.GetCurrentDirectory(), pathOfImage));
+        }
+    }
+}
